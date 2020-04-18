eedc34778ca8fcc2e62ba3d53bd337f580d6c217
SAURABH1299 <tim@gmail.com.net>
2020-04-18 19:31:13 +0530

    files added

---
+++ else_if.java
@@ -0,0 +1,22 @@
+package advanto_programs_imp;
+
+public class else_if {
+	public static void main(String[] args) 
+	{
+		int n=15;
+	if (n%2==78)
+	{
+		System.out.println("pass");
+	}
+	else if(n%6==78)
+	{
+		System.out.println("saurabh");
+	}
+	else if(n%3==45)
+	{
+		System.out.println("bidkar");
+	}
+	else
+	System.out.println("advanto");
+	}
+}
---
+++ while_loop.java
@@ -0,0 +1,23 @@
+package advanto_programs_imp;
+
+public class while_loop {
+
+public static void main(String[] args)
+{
+//int x=10;
+int y = 0;
+do
+{
+System.out.println("hello");
+y++;
+} while (y<=8);
+}
+}
+
+/*while(x<12)
+{
+	System.out.println("advanto");
+	y++;	
+}				
+}
+}*/
